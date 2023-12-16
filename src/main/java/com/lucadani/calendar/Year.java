2447eaaf3ecf2bfc224279fe8d09aeea7fa71ce9
lucadani7 <lucaionescu1998@gmail.com>
2023-12-16 18:52:13 +0200

    Initial commit

--- src/main/java/com/lucadani/calendar/Year.java
+++ src/main/java/com/lucadani/calendar/Year.java
@@ -1,5 +1,5 @@
 package com.lucadani.calendar;
 
 public enum Year {
-    Jabuary, February, March, April, May, June, July, August, September, October, November, December;
+    January, February, March, April, May, June, July, August, September, October, November, December;
 }
