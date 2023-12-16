cd830998f470acc7852370d507445b674021a589
lucadani7 <lucaionescu1998@gmail.com>
2023-12-16 18:45:06 +0200

    Initial commit

--- src/main/java/com/lucadani/Main.java
+++ src/main/java/com/lucadani/Main.java
@@ -1,7 +1,16 @@
 package com.lucadani;
 
+import com.lucadani.calendar.MonthCalendar;
+import com.lucadani.calendar.MonthCalendarInterface;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MonthCalendarInterface monthCalendar = new MonthCalendar();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Type a year: ");
+        int year = scanner.nextInt();
+        monthCalendar.printCalendar(year);
     }
 }
